433bd2bfa822116d6c81e4df3e5cd54005819c25
Mohak Sharma <mohak327@gmail.com>
2021-09-10 10:36:17 +0530

    added date time in exp1

--- src/Lab/exp1/Main.java
+++ src/Lab/exp1/Main.java
@@ -1,10 +1,18 @@
 package Lab.exp1;
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+        
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
+        LocalDateTime now = LocalDateTime.now();
+
+        System.out.println(dtf.format(now));
 
     }
 }
